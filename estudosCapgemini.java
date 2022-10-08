546e8a267fb7a22344eaf45f39e0b3db66059be7
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-10-07 23:13:38 -0300

    exemplo 1

---
+++ estudosCapgemini.java
@@ -0,0 +1,20 @@
+package capgeminiFundamentos;
+
+public class estudosCapgemini {
+
+	public static void main(String[] args) {
+		int num1 = 20, num2 = 30;
+		if(num1 < num2) {
+			System.out.println("Numero 1 é menor que o 2");
+			
+		}
+		
+		if (num1 > num2) {
+			System.out.println("numero 1 é maior que o 2");
+		} else {
+			System.out.println("numero 1 é menor que o dois");
+		}
+
+	}
+
+}
